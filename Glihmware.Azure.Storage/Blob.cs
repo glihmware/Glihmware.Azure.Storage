5dfa7662b196ce796aab4d19725ccac83f6aef8a
glihm <glihm@outlook.fr>
2020-08-01 07:11:24 +0200

    initial commit

---
+++ Glihmware.Azure.Storage/Blob.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using Azure;
+
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace Glihmware.Azure.Storage
+{
+    public static class Blob
+    {
+        /// <summary>
+        ///   Lists blob's names inside the given container.
+        ///   Flat listing, only returning the name.
+        /// </summary>
+        /// 
+        /// <param name="serviceClient"> Blob service client of storage account. </param>
+        /// 
+        /// <param name="containerName"> Container name to be listed. </param>
+        public static async Task<List<string>>
+        ListFlat
+        (
+            BlobServiceClient serviceClient,
+            string containerName
+        )
+        {
+            List<string> blobNames = new List<string>();
+
+            BlobContainerClient bcc
+                = serviceClient.GetBlobContainerClient(containerName);
+
+            await foreach (BlobItem bi in bcc.GetBlobsAsync(BlobTraits.None))
+            {
+                blobNames.Add(bi.Name);
+            }
+
+            return blobNames;
+        }
+
+
+
+
+
+        /// <summary>
+        ///   Downloads a blob.
+        /// </summary>
+        /// 
+        /// <param name="serviceClient"> Blob service client of storage account. </param>
+        /// 
+        /// <param name="containerName"> Container where the blob is stored. </param>
+        /// 
+        /// <param name="blobName"> Name of the blob to be downloaded. </param>
+        /// 
+        /// <returns> `MemoryStream` with the blob content if success, `null` otherwise. </returns>
+        public static async Task<MemoryStream>
+        Download
+        (
+            BlobServiceClient serviceClient,
+            string containerName,
+            string blobName
+        )
+        {
+            BlobContainerClient bcc = serviceClient.GetBlobContainerClient(containerName);
+            BlobClient bc = bcc.GetBlobClient(blobName);
+
+            Response r;
+            MemoryStream blobContent = new MemoryStream();
+
+            try
+            {
+                r = await bc.DownloadToAsync(blobContent);
+            }
+            catch (RequestFailedException e)
+            {
+                // Need simple logger to be used everywhere...!
+                Console.WriteLine($"{e.Message}");
+                return null;
+            }
+
+            return blobContent;
+        }
+
+
+
+        /// <summary>
+        ///   Uploads a blob.
+        /// </summary>
+        /// 
+        /// <param name="serviceClient"> Blob service client of storage account. </param>
+        /// 
+        /// <param name="containerName"> Container where the blob will be uploaded. </param>
+        /// 
+        /// <param name="blobName"> Name of the blob (including virtual folders). </param>
+        /// 
+        /// <param name="blobContent"> Blob's content. </param>
+        /// 
+        /// <returns> `true` if the blob was uploaded successfully, `false` otherwise.</returns>
+        public static async Task<bool>
+        Upload
+        (
+            BlobServiceClient serviceClient,
+            string containerName,
+            string blobName,
+            MemoryStream blobContent,
+            bool overwrite = false
+        )
+        {
+            BlobContainerClient bcc = serviceClient.GetBlobContainerClient(containerName);
+            BlobClient bc = bcc.GetBlobClient(blobName);
+
+            BlobContentInfo r;
+
+            try
+            {
+                blobContent.Position = 0;
+                r = await bc.UploadAsync(blobContent, overwrite);
+            }
+            catch (RequestFailedException e)
+            {
+                Console.WriteLine($"{e.Message}");
+                return false;
+            }
+
+            return true;
+        }
+
+
+    }
+}
---
+++ Tests/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+using Azure.Storage.Blobs;
+using Glihmware.Azure.Storage;
+
+namespace Tests
+{
+    class Program
+    {
+        async static Task Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            string cs = "DefaultEndpointsProtocol=https;AccountName=glihms2;AccountKey=IG492Wf8yC2tiM1wGzAa+qDy54T24SmIKgwrlfB5RtFdeRlhzqBWRIIA9wRRUD6lyf8BuTkkmS0y0Z+sUSYCOA==;EndpointSuffix=core.windows.net";
+
+            BlobServiceClient bsc = new BlobServiceClient(cs);
+
+            MemoryStream ms = await Blob.Download(bsc, "cont1", "img.jpg");
+            if (ms == null)
+            {
+                Console.WriteLine("Nothing retuned...");
+            }
+            else
+            {
+                File.WriteAllBytes(@"/tmp/test.jpg", ms.ToArray());
+                Console.WriteLine("Dl ok");
+            }
+
+            MemoryStream msc = new MemoryStream();
+            using (FileStream fs = new FileStream(@"/tmp/test.jpg", FileMode.Open, FileAccess.Read))
+            {
+                fs.CopyTo(msc);
+            }
+
+            bool r = await Blob.Upload(bsc, "cont1", "img2.jpg", msc);
+            Console.WriteLine(r);
+        }
+    }
+}
